ba2db667b1d4d1e2633c6b383933e6935c4b271c
PierreGatschine <pierre.gatschine@gmail.com>
2021-06-11 15:33:25 +0200

    add table users

---
+++ backend/groupomania.sql
@@ -0,0 +1,31 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users`
+(
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `lastName` varchar
+(255) NOT NULL,
+  `firstName` varchar
+(255) NOT NULL,
+  `email` varchar
+(50) NOT NULL,
+  `password` varchar
+(255) NOT NULL,
+  `moderation` int unsigned DEFAULT NULL,
+  PRIMARY KEY
+(`id`),
+  UNIQUE KEY `email`
+(`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
